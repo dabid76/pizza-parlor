3f9511d9c8973e88bb37add7523d662675fa4da0
Edith Emmings <emmings.edith@gmail.com>
2019-09-26 10:58:40 -0500

    installation

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './components/App/App';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
-import * as serviceWorker from './serviceWorker';
 
 
 const pizzaReducer = (state = [], action) => {
@@ -41,10 +40,7 @@ const reduxStore = createStore(
 
 ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
 
 
 
