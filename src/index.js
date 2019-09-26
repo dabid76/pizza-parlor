e87c9a7d8e578226ed2167ae2496070147f2e1ee
Cassidy Foust <cassidy.foust@gmail.com>
2019-09-26 10:34:10 -0500

    added redux store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import * as serviceWorker from './serviceWorker';
+
+
+const pizzaList = (state = [], action) => {
+    // if (action.type === 'LIST_PIZZAS') {
+    //     return action.payload
+    // }
+    // TODO - set book list with data from server
+    return state;
+}
+
+const reduxStore = createStore(
+    combineReducers({
+        pizzaList
+    }),
+    applyMiddleware(logger)
+);
+
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
