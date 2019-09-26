306e78d4fe9a5b4b5ce3b09b11c9dd43dfd687ce
Jake Ford <jakemford17@gmail.com>
2019-09-26 16:32:55 -0500

    Added Nav Bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-// import Home from '../Home/Home.js';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import Home from '../Home/Home.js';
 import { connect } from 'react-redux';
 import PizzaList from '../PizzaList/PizzaList';
 // import PizzaItem from '../PizzaItem/PizzaItem';
-// import OrderForm from '../OrderForm/OrderForm';
-// import Checkout from '../Checkout/Checkout';
+import OrderForm from '../OrderForm/OrderForm';
+import Checkout from '../Checkout/Checkout';
 // import CheckoutTable from '../CheckoutTable/CheckoutTable';
 // import UserInfo from '../UserInfo/UserInfo';
 
@@ -25,13 +26,29 @@ class App extends Component {
   }
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Prime Pizza</h1>
         </header>
         <br/>
-      <PizzaList/>
+        <ul class="main-nav">
+            <li class="main-nav-li">
+              <Link to="/">Home</Link>
+            </li>
+            <li class="main-nav-li">
+              <Link to="/customer-info">Order Form</Link>
+            </li>
+            <li class="main-nav-li">
+              <Link to="/checkout">Checkout</Link>
+            </li>
+          </ul>
+        <Route path="/" exact component={PizzaList}/>
+        <Route path="/customer-info" component={OrderForm}/>
+        <Route path="/checkout" component={Checkout}/>
+      {/* <PizzaList/> */}
       </div>
+      </Router>
     );
   }
 }
