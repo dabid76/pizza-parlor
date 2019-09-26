e378c710032dba83231d2c367a9ca5359ad66f6c
Edith Emmings <emmings.edith@gmail.com>
2019-09-26 14:33:51 -0500

    created the form for entering in customer info

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class OrderForm extends Component {
+
+    postOrders = () => {
+        axios.post('/api/order')
+            .then(response => {
+                console.log(response)
+            }).catch(error => {
+                console.log('error in order form post ', error)
+            })
+    }
     render() {
         return (
             <>
+                <h2>Step 2: Customer Information</h2>
+                <div className="order-form-flex">
+                    <div className="customer-info-inputs">
+                        <input placeholder="Name" /><br />
+                        <input placeholder="Street Address" /><br />
+                        <input placeholder="City" /><br />
+                        <input placeholder="Zip" /><br />
+                    </div>
+                    <ul className="customer-info-select">
+                        <li><input type="radio" />Pickup</li>
+                        <li><input type="radio" defaultChecked />Delivery</li>
+                    </ul>
+                </div>
             </>
         );
     }
