76b35323137162f26725bdadb765b1a9e31b2491
Edith Emmings <emmings.edith@gmail.com>
2019-09-27 10:03:35 -0500

    wrote functions to grab order and pizza info and send to redux

--- src/index.js
+++ src/index.js
@@ -7,27 +7,33 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
 let order = {
-    pizzas: [],
+    pizzaOrder: {},
     customerInfo: []
 }
 
 const pizzaReducer = (state = [], action) => {
     if (action.type === 'LIST_PIZZAS') {
+        console.log('payload', action.payload)
+        return action.payload
+    } else if (action.type === 'ADD_TOTAL') {
+        console.log('payload', action.payload)
         return action.payload
     }
     return state;
 }
 
 const orderReducer = (state = order, action) => {
-    if (action.type === 'ORDER__INFO_PIZZAS') {
+    if (action.type === 'ORDER_INFO_PIZZAS') {
+        console.log('payload', action.payload)
         return {
             ...state,
-            pizzas: [action.payload]
+            pizzas: action.payload
         }
-    } else if (action.type === 'ORDER__INFO_CUSTOMER') {
+    } else if (action.type === 'ORDER_INFO_CUSTOMER') {
+        console.log('payload', action.payload)
         return {
             ...state,
-            customerInfo: [action.payload]
+            customerInfo: action.payload
         }
     }
     return state;
